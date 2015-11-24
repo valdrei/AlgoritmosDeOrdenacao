fd2405d81c722d79be1d20d8f0ac970aab9b4580
Valdrei Salomão da Silva <valdreisalomao@hotmail.com>
2015-11-24 14:43:34 -0200

    Inserindo metodo insertion sort

--- src/MetodosOrdenacao/InsertionSort.java
+++ src/MetodosOrdenacao/InsertionSort.java
@@ -2,4 +2,17 @@ package MetodosOrdenacao;
 
 public class InsertionSort {
 
+	public int[] insertionSort( int[] vetInsert){
+		
+		for(int i = 1; i < vetInsert.length; i++){
+			int temp = vetInsert[i];
+			int j;
+			for(j = i-1; j >= 0 && temp < vetInsert[j]; j--)
+				vetInsert[j+1] = vetInsert[j];
+			
+			vetInsert[j+1] = temp;
+			
+		}
+		return vetInsert;
+	}
 }
