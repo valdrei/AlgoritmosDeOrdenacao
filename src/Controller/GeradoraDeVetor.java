13db967d0c7c66f4de79a0186856a1d14e0912b4
feoliv <feoliv@IBM641-L1C9V49.br.ibm.com>
2015-11-24 14:30:43 -0200

    Metodos para gerar os vetores

--- src/Controller/GeradoraDeVetor.java
+++ src/Controller/GeradoraDeVetor.java
@@ -1,21 +1,57 @@
 package Controller;
 
+import java.util.Random;
+
 public class GeradoraDeVetor {
 	
 	int[] vetor;
+	int qtd;
 	
 	public GeradoraDeVetor(int qtd, String metodo){
 		
-		switch (metodo){
+		switch (metodo){	
+			case  	"random": 
+				this.vetorRandom();
+			break;
+			case	"gropos":
+				this.vetorGroup();
+			break;
+		}
 		
-		case()
+		this.qtd=qtd;
+		
+		vetor=new int[qtd];
+		
+	}
+	
+	//metodo Random
+	private void vetorRandom(){
+		
+		Random random=new Random();
+		
+		for(int i=0;i<qtd;i++){
+			vetor[i]=random.nextInt();
+		}		
+		
+	}
+	
+	//metodo Grupal
+	private void vetorGroup(){
+		
+		int qtdvalores=0;
 		
 		
+		for(int i=0;i<qtdvalores;i++){
+			
 		}
 		
 	}
 	
-	//
+	//Retorna o vetor
+	public int[] getVetor(){
+		
+		return vetor;
+	}
 			
 	
 }
