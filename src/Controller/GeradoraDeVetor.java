0539b6b96a287d73556de35377d1e08ba57b59e8
feoliv <feoliv@IBM641-L1C9V49>
2015-11-28 19:50:06 -0200

    Metodos para retornar os vetores abaixo foram criados:
    Grupal de 10%
    Inversamente ordenado

--- src/Controller/GeradoraDeVetor.java
+++ src/Controller/GeradoraDeVetor.java
@@ -2,6 +2,8 @@ package Controller;
 
 import java.util.Random;
 
+import MetodosOrdenacao.BubbleSort;
+
 public class GeradoraDeVetor {
 	
 	int[] vetor;
@@ -9,49 +11,57 @@ public class GeradoraDeVetor {
 	
 	public GeradoraDeVetor(int qtd, String metodo){
 		
-		switch (metodo){	
-			case  	"random": 
-				this.vetorRandom();
-			break;
-			case	"gropos":
-				this.vetorGroup();
-			break;
-		}
-		
 		this.qtd=qtd;
-		
-		vetor=new int[qtd];
 		
 	}
 	
 	//metodo Random
-	private void vetorRandom(){
+	public int[] vetorRandom(){
 		
 		Random random=new Random();
+		vetor=new int[qtd];
 		
 		for(int i=0;i<qtd;i++){
 			vetor[i]=random.nextInt();
-		}		
-		
-	}
-	
-	//metodo Grupal
-	private void vetorGroup(){
-		
-		int qtdvalores=0;
-		
-		
-		for(int i=0;i<qtdvalores;i++){
-			
 		}
-		
-	}
-	
-	//Retorna o vetor
-	public int[] getVetor(){
 		
 		return vetor;
 	}
+	
+	//metodo Ordenado Inversamente
+		public int[] vetorInverOrdenado(){
+			//Cria os objetos utilizados
+			BubbleSort bubble=new BubbleSort();
+			Random random=new Random();
+			//Inicia um vetor randomico
+			vetor=this.vetorRandom();
+			//Retorna o vertor ordenado inversamente	
+			return bubble.invertBubleSort(vetor);			
+		}
+	
+	//metodo Grupal de 10%
+	public int[] vetorGroup(){
+		Random random=new Random();
+		vetor=new int[qtd];
+		
+		int j=0;
+		int contador=0;
+		int partes=qtd/10;
+		
+		for(int i=0;i<10;i++){
 			
+			int valor= random.nextInt();
+			
+			for(j=contador ;j<partes;j++){
+				
+				vetor[j]=valor;
+						
+				contador++;
+			}		
+			
+		}
+		return vetor;
+	}
+	
 	
 }
--- src/MetodosOrdenacao/BubbleSort.java
+++ src/MetodosOrdenacao/BubbleSort.java
@@ -21,4 +21,19 @@ public class BubbleSort {
 		
 		return vetBubble;
 	}
+	
+	public int[] invertBubleSort(int[] vetBubble){
+	
+		int[] resultado = vetBubble;  
+	    for (int i = vetBubble.length; i >= 1; i--) {  
+	        for (int j = 1; j < i; j++) {  
+	            if (resultado[j - 1] < vetBubble[j]) {  
+	                int aux = vetBubble[j];  
+	                resultado[j] = vetBubble[j - 1];  
+	                resultado[j - 1] = aux;  
+	            	}  
+	        	}	  
+	    	} 
+	    return vetBubble;
+	}
 }
