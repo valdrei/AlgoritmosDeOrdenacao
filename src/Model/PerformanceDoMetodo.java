bff0abd85a7437853ae23489450f67cb80960349
feoliv <feoliv@IBM641-L1C9V49>
2015-11-28 18:29:19 -0200

    Modelo de resultado de performance criado.

---
+++ src/Model/PerformanceDoMetodo.java
@@ -0,0 +1,41 @@
+package Model;
+
+public class PerformanceDoMetodo {
+	
+	private int qtdTrocas;
+	private int tempeExecucao;
+	private String nomeAlgoritmo;
+	
+	public PerformanceDoMetodo(){
+		qtdTrocas=-1;
+		tempeExecucao=-1;
+		nomeAlgoritmo="";
+	}
+
+	public int getQtdTrocas() {
+		return qtdTrocas;
+	}
+
+	public void setQtdTrocas(int qtdTrocas) {
+		this.qtdTrocas = qtdTrocas;
+	}
+
+	public int getTempeExecucao() {
+		return tempeExecucao;
+	}
+
+	public void setTempeExecucao(int tempeExecucao) {
+		this.tempeExecucao = tempeExecucao;
+	}
+
+	public String getNomeAlgoritmo() {
+		return nomeAlgoritmo;
+	}
+
+	public void setNomeAlgoritmo(String nomeAlgoritmo) {
+		this.nomeAlgoritmo = nomeAlgoritmo;
+	}
+	
+	
+
+}
